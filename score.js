28593af5ae01d1a418260a336b54cc624b5ceebd
Gabriel Low <lowgabriel@ymail.com>
2022-12-05 15:05:30 +0800

    added splitDataset function

--- score.js
+++ score.js
@@ -5,7 +5,6 @@ const k = 3;
 const onScoreUpdate = (dropPosition, bounciness, size, bucketLabel) => 
   outputs.push([dropPosition, bounciness, size, bucketLabel])
 
-
 const runAnalysis = () => {
   const bucket = _.chain(outputs)
     .map(row => [distance(row[0]), row[3]])  // [distance of dropPosition from predictionPoint, bucket]
@@ -17,10 +16,18 @@ const runAnalysis = () => {
     .last()  // get array element with highest occurrence value
     .first()  // get first element of the array (the bucket number)
     .parseInt()  // convert to int
-    .value()  // stop chain operation
+    .value();  // stop chain operation
   
-    console.log('Your point will probably fall into ' + bucket)
+    console.log('Your point will probably fall into ' + bucket);
 }
 
 // calculate distance of a point from the predictionPoint
 const distance = point => Math.abs(point - predictionPoint);
+
+// splits the dataset into a dataset for testing and for training
+const splitDataset = (data, testCount) => {
+  const shuffled = _.shuffle(data);
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+  return [testSet, trainingSet];
+}
