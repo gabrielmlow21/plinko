f7b9317d2a97244030bfa22e858dadbb162eb1c4
Gabriel Low <lowgabriel@ymail.com>
2022-12-05 14:48:43 +0800

    added comments

--- score.js
+++ score.js
@@ -5,20 +5,22 @@ const k = 3;
 const onScoreUpdate = (dropPosition, bounciness, size, bucketLabel) => 
   outputs.push([dropPosition, bounciness, size, bucketLabel])
 
+
 const runAnalysis = () => {
   const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
-    .sortBy(row => row[0])
-    .slice(0, k)
-    .countBy(row => row[1])
-    .toPairs()
-    .sortBy(row => row[1])
-    .last()
-    .first()
-    .parseInt()
-    .value()
+    .map(row => [distance(row[0]), row[3]])  // [distance of dropPosition from predictionPoint, bucket]
+    .sortBy(row => row[0])  // sort based on distance of dropPosition from predictionPoint
+    .slice(0, k)  // take first k elements
+    .countBy(row => row[1])  // count the occurrence of each bucket and returns object of objects, i.e. {bucket: occurrence}
+    .toPairs()  // converts result to array of arrays. i.e. [bucket, occurrence]
+    .sortBy(row => row[1])  // sort based on array element with occurrence value (ascending)
+    .last()  // get array element with highest occurrence value
+    .first()  // get first element of the array (the bucket number)
+    .parseInt()  // convert to int
+    .value()  // stop chain operation
   
     console.log('Your point will probably fall into ' + bucket)
 }
 
+// calculate distance of a point from the predictionPoint
 const distance = point => Math.abs(point - predictionPoint);
